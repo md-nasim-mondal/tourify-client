dd46a6d9f93f1bda8ed0fe1f3c172d09c8dcd1c3
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-12-07 20:31:50 +0600

    update folder structure

---
+++ src/app/(commonLayout)/(auth)/register/page.tsx
@@ -0,0 +1,38 @@
+import RegisterForm from "@/components/modules/auth/RegisterForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register - Tourify",
+  description: "Create a new account to start your journey with Tourify.",
+};
+
+const RegisterPage = () => {
+  return (
+    <>
+      <div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
+        <div className='w-full max-w-xl'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Create an account</CardTitle>
+              <CardDescription>
+                Enter your information below to create your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <RegisterForm />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RegisterPage;
